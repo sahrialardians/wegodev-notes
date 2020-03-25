cdf62dace8767c47f757832af27b25ee580f0e79
sahrialardians <sahrialardians@gmail.com>
2020-03-25 20:47:03 +0700

    create return respon

--- api.php
+++ api.php
@@ -27,6 +27,8 @@
           function deleteNote($id){
                $deleteNotes = $this->db->prepare("DELETE FROM note WHERE id = :id");
                $deleteNotes->execute(['id' => $id]);
+
+               return json_encode(['status' => true, 'id' => $id, 'pesan' => 'data berhasil dihapus']);
           }
 
           function updateNote($id, $title, $description){
@@ -36,6 +38,8 @@
                $getNotes->execute(['id' => $id,
                                    'title' => $title,
                                    'description' => $description]);
+
+               return json_encode(['status' => true, 'id' => $id, 'pesan' => 'data berhasil diubah']);
           }
 
           function allNotes(){
@@ -62,10 +66,10 @@
           echo $api->createNote($_POST['title'], $_POST['description']);
      }
      else if($f == "UPDATE"){
-          $api->updateNote($_POST['id'], $_POST['title'], $_POST['description']);
+          echo $api->updateNote($_POST['id'], $_POST['title'], $_POST['description']);
      }
      else if($f == "DELETE"){
-          $api->deleteNote($_POST['id']);
+          echo $api->deleteNote($_POST['id']);
      }
      else{
           if(isset($_GET['id'])){
